b7c13807a48c541272af3e5285961951bb350540
aziret <kubaaziret68@gmail.com>
2024-05-20 08:49:01 +0600

    aziret ginihs header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import './Header.scss'
-import HeaderTop from './HeaderTop'
-import HeaderBottom from './HeaderBottom'
+import React from "react";
+import "./Header.scss";
+import HeaderTop from "./HeaderTop";
+import HeaderBottom from "./HeaderBottom";
 function Header() {
   return (
     <>
-    <HeaderTop/>
-    <HeaderBottom/>
-      </>
-  )
+      <HeaderTop />
+      <HeaderBottom />
+    </>
+  );
 }
 
-export default Header
+export default Header;
--- src/components/header/HeaderBottom.jsx
+++ src/components/header/HeaderBottom.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import "./Header.scss";
+import { IoMdSearch } from "react-icons/io";
+import { SlBasket } from "react-icons/sl";
+import { RxAvatar } from "react-icons/rx";
+import { NavLink } from "react-router-dom";
 function HeaderBottom() {
   return (
-    <header className="header">
+    <header className="header mt-[1.5pc]">
       <div className="container">
         <div className="header__content flex justify-between items-center">
           <div className="header__content__logo">
@@ -16,9 +20,27 @@ function HeaderBottom() {
           <div className="header__content__search">
             <div className="header__content__search__child ">
               <input type="text" name="" id="" />
+              <IoMdSearch fontSize={60} color="white" />
             </div>
           </div>
-          <div className="header__content__nav"></div>
+          <div className="header__content__nav flex justify-between items-center gap-[30px]">
+            <div className="header__content__nav__basket flex gap-[10px]">
+              <SlBasket fontSize={45} />
+              <div className="">
+                <p>1 шт</p>
+                <p>
+                  <strong>2000 сом</strong>
+                </p>
+              </div>
+            </div>
+            <div className="header__content__nav__profile flex gap-[10px]">
+              <RxAvatar fontSize={45} />
+              <div className="flex flex-col">
+                <NavLink to={"/"}>Вход</NavLink>
+                <NavLink to={"/"}>Регистрация</NavLink>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </header>
