fe10d629bcab980d7c3f4fe3e217cbd757a3d1a1
aziret <kubaaziret68@gmail.com>
2024-05-21 17:41:56 +0600

    .

---
+++ src/components/catalog/Catalog.jsx
@@ -0,0 +1,17 @@
+import "./Catalog.scss";
+function Catalog() {
+  return (
+    <section className="catalog">
+      <div className="container">
+        <div className="catalog__content">
+          <div className="catalog__content__child">
+            <h1>{'aaa'}</h1>
+            <img src="https://yamato.kg/system/menu_item_icons/75/antifreez_original.png?1607800453" alt="" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Catalog;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../components/header/Header";
+import Catalog from "../components/catalog/Catalog";
 
 function Home() {
   return (
     <div>
       <Header />
+      <Catalog />
     </div>
   );
 }
